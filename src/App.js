1649afe3ae40cd851add818facd94252134a5f23
Martin Downes <mndowne@gmail.com>
2021-12-18 11:52:22 -0600

    Added the add movie form and am handling fetch errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect , useCallback} from 'react';
+import AddMovie from './components/AddMovie';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,12 +8,21 @@ function App() {
 
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const fetchMovieHandler = () =>  {
+    const fetchMovieHandler = useCallback(() =>  {
         setIsLoading(true);
+        setError(null);
         fetch('https://swapi.py4e.com/api/films/').then(response => {
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
             return response.json();
-            }).then(data => {
+            }).catch(error => {
+                setError(error.message);
+            setIsLoading(false);
+            })
+            .then(data => {
                 const transformedMovies = data.results.map(movieData => {
                     return {
                         id: movieData.episode_id,
@@ -24,17 +34,29 @@ function App() {
             setMovies(transformedMovies);
             setIsLoading(false);
         });
+    }, []);
+
+    const addMovieHandler = (movie) => {
+        console.log(movie);
     };
+
+    useEffect(() => {
+        fetchMovieHandler()
+    }, [fetchMovieHandler] );
 
     return (
         <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>
             <section>
                 <button onClick={fetchMovieHandler}>Fetch Movies</button>
             </section>
             <section>
                 {!isLoading &&  movies.length > 0 && <MoviesList movies={movies} /> }
-                {!isLoading &&  movies.length === 0 && <p>Found no movies. </p>}
+                {!isLoading &&  movies.length === 0 && !error && <p>Found no movies. </p>}
                 { isLoading && <p>Loading...</p> }
+                { !isLoading && error && <p>{error}</p> }
             </section>
         </React.Fragment>
     );
